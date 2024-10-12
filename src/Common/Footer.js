84b70bac2a4990cb3b136230a827b3d771adf516
dp <codewithdp2073@gmail.com>
2024-10-12 11:47:49 +0530

    footer ready

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -1,7 +1,38 @@
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import '../index.css'
+
 export default function Footer() {
     return (
         <>
-        <footer>
-            </footer>        </>
+            <footer  className="py-5 bg-dark text-white rounded-top">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h4 className="text-warning ">Registered office:</h4>
+                            Udyog Bhavan, Block No. 16,<br />
+                            4th Floor, Sector-11,<br />
+                            Gandhinagar - 382 010.<br />
+                            Tel:079-23977219, 079-23977229.<br />
+                            Fax: Not In Use<br />
+                            Toll Free Number:1800 203 1111<br />
+                            E -mail: info@gujarattourism.com
+                        </Col>
+                        <Col >
+                            <ul >
+                                <h4 className="text-warning ">To Begin Your Journey</h4>
+                                <li><a href='https://www.gujarattourism.com/central-zone/ahmedabad.html' >Ahmedabad</a></li>
+                                <li><a href='https://www.gujarattourism.com/central-zone/gandhinagar.html' className="py-2">Gandhinagar</a></li>
+                                <li><a href='https://www.gujarattourism.com/central-zone/vadodara.html' className="pb-2">Vadodara</a></li>
+                                <li><a href='https://www.gujarattourism.com/saurashtra/jamnagar.html' className="pb-2">Jamnagar</a></li>
+                                <li><a href="https://www.gujarattourism.com/south-zone/surat.html">Surat</a></li>
+                            </ul>
+                        </Col>
+                        <Col>
+                        <img className="w-auto h-75 " src="https://www.gujarattourism.com/content/dam/gujrattourism/images/other-images/gujarat-tourism-logo.png" alt="logo" />
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+        </>
     );
 }
