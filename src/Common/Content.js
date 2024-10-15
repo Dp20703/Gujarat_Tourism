059d8efac2319d876f1abd6d6edc04f80c3ff8c5
dp <codewithdp2073@gmail.com>
2024-10-15 08:38:00 +0530

    Features part added

--- src/Common/Content.js
+++ src/Common/Content.js
@@ -1,16 +1,71 @@
-import { Button, Card, Container } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 export default function Content() {
     return (
         <>
+            <Row><Col><Feature /></Col></Row>
+            <Row><Col><AboutUs /></Col></Row>
 
-            <Card className="m-3">
-                <Card.Img src="https://www.gujarattourism.com/content/gujrattourism/en/about-gujarat/_jcr_content/root/responsivegrid/pagebanner.coreimg.jpeg/1643369253714/about1.jpeg" alt="about-Gujarat" className="rounded" />
-                <Card.Header className="px-3 fs-2" >
-                    About Gujarat
-                    <Card.Subtitle className="py-1 text-muted">Know more about Gujarat State</Card.Subtitle>
-                </Card.Header>
-                <Container>
+        </>
+    );
+}
+
+function Feature() {
+    return (
+        <>
+            <Container fluid className="my-4 mx-1">
+                <h2 className="py-3 px-2">Features</h2>
+                <Row>
+                    <Col>
+                        <Card className="bg-primary text-white m-2">
+                            <Card.Header>
+
+                                <i className="fs-3 ri-map-fill" />
+                                <span className="fs-3 mx-2"> Interactive Maps</span>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text className="fs-5"> Detailed maps with points of interest, routes, and interactive elements to help users plan their trips2.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card></Col>
+                    <Col>
+                        <Card className="bg-primary text-white m-2">
+                            <Card.Header>
+                                <i className="fs-3 ri-notification-fill" />
+                                <span className="fs-3 mx-2">Real-Time Updates</span>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text className="fs-5">Notifications about weather, travel advisories, and other important information to keep users informed1.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card></Col>
+                    <Col>
+                        <Card className="bg-primary text-white m-2">
+                            <Card.Header>
+                                <i className="fs-3 ri-chat-4-fill" />
+                                <span className="fs-3 mx-2">Live Chat Support</span>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text className="fs-5">Instant assistance through live chat to answer queries and provide support.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card></Col>
+
+                </Row>
+            </Container>
+        </>
+    );
+}
+function AboutUs() {
+    return (
+        <>
+            <Container fluid >
+                <Card className="my-4 mx-2">
+                    <Card.Img src="https://www.gujarattourism.com/content/gujrattourism/en/about-gujarat/_jcr_content/root/responsivegrid/pagebanner.coreimg.jpeg/1643369253714/about1.jpeg" alt="about-Gujarat" className="rounded" />
+                    <Card.Header className="px-3 fs-2" >
+                        About Gujarat
+                        <Card.Subtitle className="py-1 text-muted">Know more about Gujarat State</Card.Subtitle>
+                    </Card.Header>
                     <Card.Body>
                         <Card.Text className="mt-5 fs-6 ">Aavo Padharo, words of welcome in the language of Gujarat because it is here that these words ring truly and the guest is 'God' and the people of Gujarat are gregariously friendly, inviting and will entice you to come again and again.<br /><br />
 
@@ -20,8 +75,11 @@ export default function Content() {
                         </Card.Text>
                         <Button variant="dark" href="https://www.gujarattourism.com/about-gujarat.html">Know more</Button>
                     </Card.Body>
-                </Container>
-            </Card>
+                </Card>
+                <Card>
+
+                </Card>
+            </Container>
         </>
     );
 }
